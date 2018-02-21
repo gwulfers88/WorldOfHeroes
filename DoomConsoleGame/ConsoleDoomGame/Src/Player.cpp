fdf7fea46f96f84dd1d4a88045dd487f71a1c39a
DAVIN\DRoss <davinross0115@gmail.com>
2018-02-20 20:37:16 -0800

    Almost finished weapon class. Still need to finish addAmmo function.

--- DoomConsoleGame/ConsoleDoomGame/Src/Player.cpp
+++ DoomConsoleGame/ConsoleDoomGame/Src/Player.cpp
@@ -1,54 +1,38 @@
+/**********************************************************
+File Name: Player.cpp
+Start Date: 01/23/2018
+Mod Date: 02/20/2018
+Creator Name: Davin Ross
+Contributors Name: Davin Ross
+===========================================================
+Description:
+	This file is the source file for the Player Class
+===========================================================
+**********************************************************/
+
 #include "Player.h"
-
-
 
 Player::Player(i8* name)
 	: _health(0),
 	_armor(0),
 	_maxHealth(100),
-	_maxArmor(100), 
-	_curWeaponsIndex(FIST), 
+	_maxArmor(100),
+	_curWeaponsIndex(FIST),
 	_maxWeapons(2) {
-
-	setName(name);
 
 	// Set All Weapons to 0 Ammo
 	for (i32 i = 0; i < _maxWeapons; i++) {
-		_ammo[i] = 0;
+		_weapons[i].setAmmo(0);
 	}
 
-	// Weapon Inventory
-	_weapons[0] = "FIST";
-	_weapons[1] = "PISTOL";
-
-	// Max Ammo
-	_maxAmmo[0] = 0;
-	_maxAmmo[1] = 50;
+	setName(name);
 }
-
 
 Player::~Player() {}
 
 // Setter Functions
 void Player::setName(i8* name) {
 
-}
-
-void Player::setAmmo(i32 ammo) {
-	if (_ammo < 0) {
-
-		// Makes Sure to Add Ammo to Weapons That Use Ammo
-		if (_curWeaponsIndex != FIST) {
-			_ammo[_curWeaponsIndex] = ammo; // Set New Ammo
-
-			// Makes Sure That Ammo Does Not Exceed Max Ammo
-			if (_ammo[_curWeaponsIndex] > _maxAmmo[_curWeaponsIndex]) {
-
-				// If Ammo Does Exceed MaxAmmo, Set Ammo to Max Ammo
-				_ammo[_curWeaponsIndex] = _maxAmmo[_curWeaponsIndex];
-			}
-		}
-	}
 }
 
 void Player::setArmor(i32 armor) {
@@ -70,16 +54,12 @@ void Player::setHealth(i32 health) {
 }
 
 void Player::setCurWeapons(WEAPONS newWeaponsIndex) {
-	_curWeaponsIndex = newWeaponsIndex;
+	_curWeaponsIndex = newWeaponsIndex; // _curWeaponIndex does not exist in player.h
 }
 
 // Getter Functions
 i8* Player::getName() const {
 	return _name;
-}
-
-i32 Player::getAmmo() const {
-	return _ammo[_curWeaponsIndex];
 }
 
 i32 Player::getArmor() const {
@@ -90,11 +70,11 @@ i32 Player::getHealth() const {
 	return _health;
 }
 
-i32 Player::getMaxAmmo() const {
-	return _maxAmmo[_curWeaponsIndex];
-}
-
-i8* Player::getCurWeapons() const {
+Weapons Player::getCurWeapons() const {
 	return _weapons[_curWeaponsIndex];
 }
+
+
+
+
 
--- DoomConsoleGame/ConsoleDoomGame/Src/Player.h
+++ DoomConsoleGame/ConsoleDoomGame/Src/Player.h
@@ -1,5 +1,19 @@
-#pragma once
+/**********************************************************
+File Name: Player.h
+Start Date: 01/23/2018
+Mod Date: 02/20/2018
+Creator Name: Davin Ross
+Contributors Name: Davin Ross
+===========================================================
+Description: 
+	This file is the header file for the Player Class
+===========================================================
+**********************************************************/
+#ifndef PLAYER_H
+#define PLAYER_H
+
 #include "platform.h"
+#include "Weapons.h"
 
 // Weapons Index
 enum WEAPONS {
@@ -14,28 +28,22 @@ public:
 	~Player(void);
 
 	// Setters
-	void setAmmo(i32);
 	void setHealth(i32);
 	void setArmor(i32);
 	void setCurWeapons(WEAPONS newWeaponsIndex);
-
+	
 	// Getters
-	i32 getAmmo() const;
 	i32 getHealth() const;
 	i32 getArmor() const;
-	i32 getMaxAmmo() const;
 	i8* getName() const;
-	i8* getCurWeapons() const;
-
+	Weapons getCurWeapons() const;
+	
 private:
-
-	// Current Ammo, Health and Armor 
-	i32 _ammo[2];
+	// Current Health and Armor 	
 	i32 _health;
 	i32 _armor;
 
-	// Max Ammo, Health and Armor
-	i32 _maxAmmo[2];
+	// Max Health and Armor
 	i32 _maxHealth;
 	i32 _maxArmor;
 
@@ -45,5 +53,6 @@ private:
 	// Weapons Slots
 	WEAPONS _curWeaponsIndex; // Curent Index
 	i32 _maxWeapons; // Max Number of Weapons
-	i8* _weapons[2]; // Weapons Array
+	Weapons _weapons[2]; // Weapons Array
 };
+#endif
